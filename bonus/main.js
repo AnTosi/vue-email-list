1afa1def0a70c278816e10c314ea70d04cdd07c2
AnTosi <andrea.tosi.uni@gmail.com>
2021-11-09 15:30:20 +0100

    Fatto Bonus? ho messo before mount invece che mounted

---
+++ bonus/main.js
@@ -0,0 +1,33 @@
+// Descrizione:
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+// ciclo v-for con list item all'interno, axios.get per prendere i dati dal database
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+// fatto bonus?
+
+
+const app = new Vue ({
+    el: "#root",
+
+    data: {
+
+        emails: [
+            
+        ],
+    },
+
+    beforeMount(){
+        for (let i = 0; i < 10; i++) {         
+            axios
+            .get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+            .then(resp => {
+                console.log(resp);
+                this.emails.push(JSON.stringify((resp.data.response)));
+                console.log(this.emails);
+            })            
+        }
+    }
+})
