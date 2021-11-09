5a097540d8250e3c60b7a0a802d74f54215d89e3
AnTosi <andrea.tosi.uni@gmail.com>
2021-11-09 15:24:44 +0100

    fastto normale

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 // Attraverso l'apposita API di Boolean
 // https://flynn.boolean.careers/exercises/api/random/mail
 // // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
-// ciclo for con list item all'interno, axios.get per prendere i dati dal database
+
+// ciclo v-for con list item all'interno, axios.get per prendere i dati dal database
 // Bonus
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 
@@ -11,15 +12,21 @@ const app = new Vue ({
     el: "#root",
 
     data: {
-        email: "",
+
+        emails: [
+            
+        ],
     },
 
     mounted(){
-        axios
-        .get(`https://flynn.boolean.careers/exercises/api/random/mail`)
-        .then(resp => {
-            console.log(resp);
-            this.email =(resp.data.response);
-        })
+        for (let i = 0; i < 10; i++) {         
+            axios
+            .get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+            .then(resp => {
+                console.log(resp);
+                this.emails.push(JSON.stringify((resp.data.response)));
+                console.log(this.emails);
+            })            
+        }
     }
 })
