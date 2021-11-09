41b9311dcf4f59c8163d994d89ccbb36c521bd44
AnTosi <andrea.tosi.uni@gmail.com>
2021-11-09 17:33:00 +0100

    messo moutned invece di before mount

--- bonus/main.js
+++ bonus/main.js
@@ -20,7 +20,7 @@ const app = new Vue ({
         ],
     },
 
-    beforeMount(){
+    mounted(){
         for (let i = 0; i < 10; i++) {         
             axios
             .get(`https://flynn.boolean.careers/exercises/api/random/mail`)
